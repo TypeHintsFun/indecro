81554161fe3d2b4de4b87aafcbc60d6dd050ae68
TypeHintsFun <mikhailsmnv22@gmail.com>
2023-05-25 10:32:32 +0800

    Typehints fix

--- indecro/api/scheduler.py
+++ indecro/api/scheduler.py
@@ -13,57 +13,46 @@ from indecro.api.task import Task
 from indecro.api.rules import Rule
 
 
-@dataclass
-class Job:
-    task: Task
-    rule: Rule
-    next_run_time: datetime
-    name: Optional[str] = None
-    scheduler: Optional[Scheduler] = None
-
-    @abstractmethod
-    async def schedule(self) -> Any:
-        raise NotImplementedError()
-
-
 class Scheduler(Protocol):
-    @abstractmethod
-    def task(
-            self,
-            rule: Rule,
+    if TYPE_CHECKING:
+        @abstractmethod
+        def task(
+                self,
+                rule: Rule,
 
                 daemonize: RunAs = RunAs.FUNCTION,
 
-            name: Optional[str] = None,
+                name: Optional[str] = None,
 
-            *args,
-            **kwargs
-    ):
-        raise NotImplementedError()
+                *args,
+                **kwargs
+        ):
+            raise NotImplementedError()
 
-    @abstractmethod
-    def add_job(
-            self,
-            task: Task,
-            rule: Rule,
+        @abstractmethod
+        def add_job(
+                self,
+                task: Task,
+                rule: Rule,
 
                 daemonize: RunAs = RunAs.FUNCTION,
 
-            name: Optional[str] = None,
+                name: Optional[str] = None,
 
-            *args,
-            **kwargs
-    ) -> Union[Job, Awaitable[Job]]:
-        raise NotImplementedError()
+                *args,
+                **kwargs
+        ) -> Union[Job, Awaitable[Job]]:
+            raise NotImplementedError()
 
-    @abstractmethod
-    async def execute_job(self, job: Job, reschedule: bool = True):
-        raise NotImplementedError()
+    if TYPE_CHECKING:
+        @abstractmethod
+        async def execute_job(self, job: Job, reschedule: bool = True):
+            raise NotImplementedError()
 
-    @staticmethod
-    @abstractmethod
-    def schedule_job(job: Job):
-        raise NotImplementedError()
+        @staticmethod
+        @abstractmethod
+        def schedule_job(job: Job):
+            raise NotImplementedError()
 
     @abstractmethod
     async def stop(self):
