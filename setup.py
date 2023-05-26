357f00d6b6c7cb035c8d4a1437333a687968dd64
TypeHintsFun <mikhailsmnv22@gmail.com>
2023-05-26 16:26:48 +0800

    PyPi update

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ this_directory = path.abspath(path.dirname(__file__))
 setup(
     name='indecro',
     description='Python scheduler with task independency from scheduler, executor and others',
-    version='0.102',
+    version='0.13',
     url='https://github.com/TypeHintsFun/indecro',
     author='TypeHintsFun',
     author_email='typehintsfun@gmail.com',
