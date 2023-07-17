a22c9c87bd5cd0dc21fd56ccb1278e9b68e6f036
TypeHintsFun <typehintsfun@gmail.com>
2023-07-17 20:43:27 +0800

    Named task example no longer looks like it was written by asshole

--- examples/named.py
+++ examples/named.py
@@ -1,10 +1,11 @@
 import asyncio
-from datetime import timedelta
+from datetime import timedelta, datetime
 
 from indecro import Scheduler
 from indecro.executor import Executor
+from indecro.job import Job
 from indecro.storage.memory_storage import MemoryStorage
-from indecro.rules import RunEvery
+from indecro.rules import RunEvery, RunOnce
 
 scheduler = Scheduler(
     executor=Executor(),
@@ -12,18 +13,18 @@ scheduler = Scheduler(
 )
 
 
-async def foo(func_name: str):
+async def foo(job_id: str):
     print(1)
-    scheduler.remove_job(func_name)
+    # Remove task by id
+    scheduler.remove_job(job_id)
 
 
-async def main():
-    scheduler.add_job(
-        task=foo,
-        rule=RunEvery(timedelta(seconds=1)),
-        func_name="func_name"
-    )
-    await scheduler.run()
+scheduler.add_job(
+    task=foo,
+    rule=RunEvery(timedelta(seconds=1)),
+    id='job_id',  # Job id for indecro
 
+    job_id='job_id'  # Same job id, that will be provided into task callable
+)
 
-asyncio.run(main())
+scheduler.start()
